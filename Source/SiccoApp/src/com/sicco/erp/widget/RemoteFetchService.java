9ae79fcf4bba440462a7d8e52029960f9629bd96
ntt.s2.102 <ntt.s2.102@gmail.com>
2014-10-18 11:36:44 +0700

    Update RemoteFetchService

--- Source/SiccoApp/src/com/sicco/erp/widget/RemoteFetchService.java
+++ Source/SiccoApp/src/com/sicco/erp/widget/RemoteFetchService.java
@@ -2,7 +2,10 @@ package com.sicco.erp.widget;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -10,19 +13,23 @@ import org.json.JSONObject;
 import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.androidquery.AQuery;
 import com.androidquery.callback.AjaxCallback;
 import com.androidquery.callback.AjaxStatus;
+import com.sicco.erp.http.HTTPHandler;
+import com.sicco.erp.manager.SessionManager;
 import com.sicco.erp.model.TatCaCongViec;
+import com.sicco.erp.service.GetNotificationService.NotificationAsync;
 
 public class RemoteFetchService extends Service {
 
 	private int appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 	private AQuery aquery;
-	private String remoteJsonUrl = "http://apis.mobile.vareco.vn/sicco/congviec_old.php";
+	private String remoteJsonUrl = "http://apis.mobile.vareco.vn/sicco/congviec.php";
 
 	public static ArrayList<ListItem> listItemList;
 	JSONArray congviec = null;
@@ -31,87 +38,66 @@ public class RemoteFetchService extends Service {
 	public IBinder onBind(Intent arg0) {
 		return null;
 	}
-
-	/*
-	 * Retrieve appwidget id from intent it is needed to update widget later
-	 * initialize our AQuery class
-	 */
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		if (intent.hasExtra(AppWidgetManager.EXTRA_APPWIDGET_ID))
 			appWidgetId = intent.getIntExtra(
 					AppWidgetManager.EXTRA_APPWIDGET_ID,
 					AppWidgetManager.INVALID_APPWIDGET_ID);
-		aquery = new AQuery(getBaseContext());
-		fetchDataFromWeb();
+		SessionManager session = SessionManager.getInstance(getApplicationContext());
+		String token = session.getUserDetails().get(SessionManager.KEY_TOKEN);
+		if (token != null && token.length() > 0) {
+			new GetCongViec().execute(token, "aew", "2");
+		}
 		return super.onStartCommand(intent, flags, startId);
 	}
 
-	/**
-	 * method which fetches data(json) from web aquery takes params
-	 * remoteJsonUrl = from where data to be fetched String.class = return
-	 * format of data once fetched i.e. in which format the fetched data be
-	 * returned AjaxCallback = class to notify with data once it is fetched
-	 */
-	private void fetchDataFromWeb() {
-		aquery.ajax(remoteJsonUrl, String.class, new AjaxCallback<String>() {
-			@Override
-			public void callback(String url, String result, AjaxStatus status) {
-				processResult(result);
-				super.callback(url, result, status);
-			}
-		});
-	}
+	public class GetCongViec extends AsyncTask<String, Void, String>{
+		@Override
+		protected String doInBackground(String... params) {
+			String token = params[0];
+			String username = params[1];
+			String page = params[2];
+			
+			Log.d("TuNT", "token " + token);
+			HTTPHandler handler = new HTTPHandler();
+			List<NameValuePair> nameValue = new ArrayList<NameValuePair>();
+			nameValue.add(new BasicNameValuePair("token", token));
+			nameValue.add(new BasicNameValuePair("username", username));
+			nameValue.add(new BasicNameValuePair("page", page));
 
-	/**
-	 * Json parsing of result and populating ArrayList<ListItem> as per json
-	 * data retrieved from the string
-	 */
-	private void processResult(String result) {
-		Log.i("Resutl",result);
-		listItemList = new ArrayList<ListItem>();
-		try {
-//			JSONArray jsonArray = new JSONArray(result);
-//			int length = jsonArray.length();
-//			for (int i = 0; i < length; i++) {
-//				JSONObject jsonObject = jsonArray.getJSONObject(i);
-//				ListItem listItem = new ListItem();
-//				listItem.heading = jsonObject.getString("heading");
-//				listItem.content = jsonObject.getString("content");
-//				listItem.imageUrl = jsonObject.getString("imageUrl");
-//				Log.i("Heading",listItem.heading);
-//				Log.i("Content",listItem.content);
-//				Log.i("imageUrl",listItem.imageUrl);
-//				listItemList.add(listItem);
-//			}
-			JSONObject jsonObj = new JSONObject(result);
-			congviec = jsonObj.getJSONArray("row");
-
-			// looping through All Contacts
-			for (int i = 0; i < congviec.length(); i++) {
-				JSONObject c = congviec.getJSONObject(i);
-
-				String id = c.getString("id");
-				String title = c.getString("ten_cong_viec");
-				String han_cuoi = c.getString("ngay_ket_thuc");
-				ListItem listItem = new ListItem();
-				listItem.id = id;
-				listItem.title = title;
-				listItem.han_cuoi = han_cuoi;
-				listItemList.add(listItem);
-			}
-
-		} catch (JSONException e) {
-			e.printStackTrace();
+			String jsonCode = handler.makeHTTPRequest(remoteJsonUrl,HTTPHandler.POST, nameValue);
+			return jsonCode;
 		}
-		populateWidget();
-	}
+		@Override
+		protected void onPostExecute(String result) {
+			Log.d("TuNT", result);
+			listItemList = new ArrayList<ListItem>();
+			try {
+				JSONObject jsonObj = new JSONObject(result);
+				congviec = jsonObj.getJSONArray("row");
 
-	/**
-	 * Method which sends broadcast to WidgetProvider
-	 * so that widget is notified to do necessary action
-	 * and here action == WidgetProvider.DATA_FETCHED
-	 */
+				for (int i = 0; i < congviec.length(); i++) {
+					JSONObject c = congviec.getJSONObject(i);
+
+					String id = c.getString("id");
+					String title = c.getString("ten_cong_viec");
+					String han_cuoi = c.getString("ngay_ket_thuc");
+					ListItem listItem = new ListItem();
+					listItem.id = id;
+					listItem.title = title;
+					listItem.han_cuoi = han_cuoi;
+					listItemList.add(listItem);
+				}
+
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+			populateWidget();
+			super.onPostExecute(result);
+		}
+		
+	}
 	private void populateWidget() {
 
 		Intent widgetUpdateIntent = new Intent();

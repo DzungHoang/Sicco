e1cc5a96e15a8fbfefb16511ee8416dd7783f637
luandt1005 <luandt1005@gmail.com>
2014-10-30 16:51:16 +0700

    LuanDT
    Update ChiTietCongViecActivity

--- Source/SiccoApp/src/com/sicco/erp/ChiTietCongViecActivity.java
+++ Source/SiccoApp/src/com/sicco/erp/ChiTietCongViecActivity.java
@@ -1,6 +1,7 @@
 package com.sicco.erp;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
@@ -10,10 +11,6 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.sicco.erp.adapter.ThaoLuanAdapter;
-import com.sicco.erp.http.HTTPHandler;
-import com.sicco.erp.model.ThaoLuan;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
@@ -21,20 +18,29 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.sicco.erp.adapter.ThaoLuanAdapter;
+import com.sicco.erp.http.HTTPHandler;
+import com.sicco.erp.manager.SessionManager;
+import com.sicco.erp.model.ThaoLuan;
+
 public class ChiTietCongViecActivity extends Activity {
 
 	ProgressDialog pDialog;
 	String url_congviec = "http://apis.mobile.vareco.vn/sicco/congviec_old.php";
-	JSONArray thaoluan = null, row = null;
+	String url_thaoluan = "http://apis.mobile.vareco.vn/sicco/thaoluan.php";
+	JSONArray thaoluan = null, row = null, success = null;
 	ArrayList<HashMap<String, String>> thaoLuanList;
 	ArrayList<ThaoLuan> mThaoLuan;
 	ListView mListView;
@@ -43,6 +49,8 @@ public class ChiTietCongViecActivity extends Activity {
 	final String cap_nhat_tien_do[] = { "0%", "20%", "40%", "60%", "80%",
 			"100%" };
 	ImageView imgAnhDaiDien;
+	ImageView imgSend;
+	EditText edtThaoLuan;
 	TextView tvTenCongViec;
 	TextView tvNguoiGiao;
 	TextView tvNguoiThucHien;
@@ -51,12 +59,39 @@ public class ChiTietCongViecActivity extends Activity {
 	TextView tvTongHopBaoCao;
 	String id, tenCongViec, tinhTrang, tienDo, nguoiThucHien, phongBan,
 			loaiCongViec, ngayKetThuc, duAn, mucUuTien, nguoiDuocXem,
-			nguoiGiao, moTa, tongHopBaoCao, Url;
+			nguoiGiao, moTa, tongHopBaoCao, Url, token, username, thoigian,
+			noidungthaoluan, updateTienDo, kqUpdateTienDo;
+	StringBuilder timeThaoLuan;
+
+	private int minute;
+	private int hour;
+	private int date;
+	private int months;
+	private int years_now;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// TuNT
+		setContentView(R.layout.activity_chi_tiet_cong_viec);
+
+		SessionManager sessionManager = SessionManager
+				.getInstance(ChiTietCongViecActivity.this);
+		token = sessionManager.getUserDetails().get(SessionManager.KEY_TOKEN);
+		username = sessionManager.getUserDetails().get(SessionManager.KEY_NAME);
+
+		final Calendar c = Calendar.getInstance();
+		minute = c.get(Calendar.MINUTE);
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		date = c.get(Calendar.DATE);
+		months = c.get(Calendar.MONTH);
+		years_now = c.get(Calendar.YEAR);
+
+		timeThaoLuan = new StringBuilder().append(padding_str(hour))
+				.append(":").append(padding_str(minute)).append("  ")
+				.append(padding_str(date)).append("-")
+				.append(padding_str(months)).append("-")
+				.append(padding_str(years_now));
+		thoigian = new String(timeThaoLuan);
 		Intent intent = getIntent();
 		id = intent.getStringExtra("id");
 		if (id != null) {
@@ -67,14 +102,11 @@ public class ChiTietCongViecActivity extends Activity {
 							TatCaCongViecActivity.class);
 					startActivity(intent2);
 				}
-				Toast.makeText(getApplicationContext(),
-						"ID: " + intent.getStringExtra("id"),
-						Toast.LENGTH_LONG).show();
+//				Toast.makeText(getApplicationContext(),
+//						"ID: " + intent.getStringExtra("id"), Toast.LENGTH_LONG)
+//						.show();
 			}
 		}
-		// End TuNT
-		setContentView(R.layout.activity_chi_tiet_cong_viec);
-
 		tenCongViec = intent.getStringExtra("ten_cong_viec");
 		tinhTrang = intent.getStringExtra("tinh_trang");
 		tienDo = intent.getStringExtra("tien_do");
@@ -103,18 +135,42 @@ public class ChiTietCongViecActivity extends Activity {
 		tvTongHopBaoCao = (TextView) findViewById(R.id.tv_chi_tiet_cv_tong_hop_bao_cao);
 		tvTongHopBaoCao.setText(tongHopBaoCao);
 
-		DisplayMetrics displayMetrics = new DisplayMetrics();
-		getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-		int lvWidth = displayMetrics.widthPixels * 10 / 10;
-		int lvHeight = displayMetrics.heightPixels * 6 / 10;
+		// //////////
+		imgSend = (ImageView) findViewById(R.id.img_send);
+		edtThaoLuan = (EditText) findViewById(R.id.edt_thao_luan);
+		imgSend.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				if (TextUtils.isEmpty(edtThaoLuan.getText())) {
+					edtThaoLuan.setError(getResources().getString(
+							R.string.thong_bao_rong));
+					return;
+				} else {
+					noidungthaoluan = edtThaoLuan.getText().toString();
+					Log.d("LuanDT", "noidungthaoluan : " + noidungthaoluan);
+					new GetThaoLuan().execute(token, username, id, thoigian,
+							noidungthaoluan);
+				}
+
+			}
+		});
+		// //////////
 
 		thaoLuanList = new ArrayList<HashMap<String, String>>();
-		new GetThaoLuan().execute();
+		new GetThaoLuan().execute(token, username);
 		mThaoLuan = new ArrayList<ThaoLuan>();
 		mListView = (ListView) findViewById(R.id.lv_thao_luan);
 		mAdapter = new ThaoLuanAdapter(getBaseContext(),
 				R.layout.item_lv_thao_luan, mThaoLuan);
 		mListView.setAdapter(mAdapter);
+	}
+
+	private static String padding_str(int c) {
+		if (c >= 10)
+			return String.valueOf(c);
+		else
+			return "0" + String.valueOf(c);
 	}
 
 	private class GetThaoLuan extends AsyncTask<String, Void, String> {
@@ -132,19 +188,19 @@ public class ChiTietCongViecActivity extends Activity {
 
 		@Override
 		protected String doInBackground(String... arg0) {
-			// Creating service handler class instance
 			HTTPHandler handler = new HTTPHandler();
 
-			// Making a request to url and getting response
 			List<NameValuePair> valuePairs = new ArrayList<NameValuePair>();
-			valuePairs.add(new BasicNameValuePair("id", id));
-			valuePairs.add(new BasicNameValuePair("tien_do", tienDo));
-			// Making a request to url and getting response
+			valuePairs.add(new BasicNameValuePair("token", token));
+			valuePairs.add(new BasicNameValuePair("ten_nguoi_thao_luan",
+					username));
+			valuePairs.add(new BasicNameValuePair("id_cong_viec", id));
+			valuePairs.add(new BasicNameValuePair("thoi_gian", thoigian));
+			valuePairs.add(new BasicNameValuePair("noi_dung", noidungthaoluan));
 			String ret = handler.makeHTTPRequest(url_congviec,
 					HTTPHandler.POST, valuePairs);
 
-			Log.d("LuanDT", "POST : " + valuePairs);
-
+			Log.d("LuanDT", "POST-ThaoLuan : " + valuePairs);
 			return ret;
 		}
 
@@ -211,7 +267,7 @@ public class ChiTietCongViecActivity extends Activity {
 								@Override
 								public void onClick(DialogInterface dialog,
 										int which) {
-									tienDo = cap_nhat_tien_do[which];
+									updateTienDo = cap_nhat_tien_do[which];
 								}
 							})
 					.setCancelable(false)
@@ -221,7 +277,8 @@ public class ChiTietCongViecActivity extends Activity {
 								@Override
 								public void onClick(DialogInterface dialog,
 										int which) {
-
+									new GetTienDoCV().execute(token, username,
+											id, updateTienDo);
 								}
 							})
 					.setNegativeButton(
@@ -241,6 +298,29 @@ public class ChiTietCongViecActivity extends Activity {
 		case R.id.action_edit:
 			Intent intent = getIntent();
 			intent.setClass(getApplicationContext(), SuaCongViecActivity.class);
+
+			intent.putExtra("id", id);
+			intent.putExtra("ten_cong_viec", tenCongViec);
+			intent.putExtra("tinh_trang", tinhTrang);
+			intent.putExtra("tien_do", tienDo);
+			intent.putExtra("nguoi_thuc_hien", nguoiThucHien);
+			intent.putExtra("phong_ban", phongBan);
+			intent.putExtra("loai_cong_viec", loaiCongViec);
+			intent.putExtra("ngay_ket_thuc", ngayKetThuc);
+			intent.putExtra("du_an", duAn);
+			intent.putExtra("muc_uu_tien", mucUuTien);
+			intent.putExtra("nguoi_duoc_xem", nguoiDuocXem);
+			intent.putExtra("nguoi_giao", nguoiGiao);
+			intent.putExtra("mo_ta", moTa);
+			intent.putExtra("tong_hop_bao_cao", tongHopBaoCao);
+			intent.putExtra("Url", Url);
+
+			Toast.makeText(
+					getApplicationContext(),
+					"id:" + intent.getStringExtra("id").toString()
+							+ "ten_cong_viec:"
+							+ intent.getStringExtra("ten_cong_viec").toString(),
+					0).show();
 			startActivity(intent);
 			break;
 
@@ -248,5 +328,54 @@ public class ChiTietCongViecActivity extends Activity {
 			break;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	// -------------------------cap nhat tien do-------------------//
+
+	private class GetTienDoCV extends AsyncTask<String, Void, String> {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(ChiTietCongViecActivity.this);
+			pDialog.setMessage(getResources().getString(R.string.vui_long_doi));
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			HTTPHandler handler = new HTTPHandler();
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			nameValuePairs.add(new BasicNameValuePair("token", token));
+			nameValuePairs.add(new BasicNameValuePair("username", username));
+			nameValuePairs.add(new BasicNameValuePair("id_cong_viec", id));
+			nameValuePairs.add(new BasicNameValuePair("tien_do", updateTienDo));
+			String ret = handler.makeHTTPRequest(url_congviec,
+					HTTPHandler.POST, nameValuePairs);
+			Log.d("LuanDT", "POST-GetTienDoCV : " + nameValuePairs);
+			return ret;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			super.onPostExecute(result);
+			if (pDialog.isShowing())
+				pDialog.dismiss();
+			if (result != null) {
+				try {
+					JSONObject jsonObject = new JSONObject(result);
+//					success = jsonObject.getJSONArray("success");
+//					for (int i = 0; i < success.length(); i++) {
+//						JSONObject kq = success.getJSONObject(i);
+//						kqUpdateTienDo = kq.getString("success");
+//					}
+//					Log.d("LuanDT", "kqUpdateTienDo : " + kqUpdateTienDo);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			} else {
+				Log.e("LuanDT", "Error!");
+			}
+		}
 	}
 }
